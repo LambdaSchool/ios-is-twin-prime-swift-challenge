979e6183ea72411458315b77d0cab989e79f9e1c
Cody Morley <codymorleyinc@gmail.com>
2020-05-20 09:00:06 -0700

    almost got theres

--- isTwinPrime.playground/Contents.swift
+++ isTwinPrime.playground/Contents.swift
@@ -1,5 +1,44 @@
 import UIKit
 
+func isPrime(_ number: Int) -> Bool {
+    var count: Int = 0
+    
+    for i in 2..<number {
+        if number % i == 0 {
+            count += 1
+        }
+    }
+    
+    if count > 0 {
+        return false
+    } else {
+        return true
+    }
+}
+
+func isTwinPrime(_ number: Int) -> Bool {
+    var twoUp: Int = (number + 2)
+    var twoDown: Int = (number + -2)
+    
+    var twoUpBool: Bool = {
+        return isPrime(twoUp)
+    }
+    
+    var twoDownBool: Bool = {
+        return isPrime(twoDown)
+    }
+    
+    if isPrime(number) == false {
+        return false
+    } else if twoUpBool == true || twoDownBool == true {
+        return true
+    } else {
+        return false
+    }
+    
+}
+
+
 
 
 
@@ -17,14 +56,14 @@ import UIKit
 
 
 //Test cases
-//isTwinPrime(2)   // false
-//isTwinPrime(10)  // false
-//isTwinPrime(9)   // false
-//isTwinPrime(4)   // false
-//isTwinPrime(5)   // true
-//isTwinPrime(7)   // true
-//isTwinPrime(23)  // false
-//isTwinPrime(37)  // false
-//isTwinPrime(883) // true
-//isTwinPrime(907) // false
-//isTwinPrime(169) // false
+isTwinPrime(2)   // false
+isTwinPrime(10)  // false
+isTwinPrime(9)   // false
+isTwinPrime(4)   // false
+isTwinPrime(5)   // true
+isTwinPrime(7)   // true
+isTwinPrime(23)  // false
+isTwinPrime(37)  // false
+isTwinPrime(883) // true
+isTwinPrime(907) // false
+isTwinPrime(169) // false
